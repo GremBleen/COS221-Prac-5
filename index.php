ff0c0301313f562f142e48c6078ca1cfbf9ecc65
u22738917 <u22738917@tuks.co.za>
2023-05-29 16:00:04 +0200

    Initial Commit - php files added and navigation bar created

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$_SESSION["loggedIn"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Wines - GrapeVine</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<?php
+// This will be the Wines tab
+require_once("php/navbar.php")
+?>
+</body>
+</html>
---
+++ wineries.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_SESSION["loggedIn"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Wineries - GrapeVine</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<?php
+require_once("php/navbar.php")
+?>
+</body>
+</html>
