eaecfb4d7ec20b4a90cb2f68ca5b44457139d8ee
Morgan90-B <u18103007@tuks.co.za>
2023-05-31 23:12:25 +0200

    Login and register page

--- php/login.php
+++ php/login.php
@@ -1,1 +1,26 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Login</title>
+</head>
+<body class="container">
+    <div class="logo"><img src="../img/GRAPEVINE.png" alt="grapevine logo"></div>
+    <div class="register">
+        <h1 class="formname">Login</h1>
+        <form action="php/login.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Enter username" required>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter password" required>
+            <button type="submit" name="register" id="register">Login</button>
+        </form>
+        <p>Don't have an account? <a href="signup.php">Register</a></p>
+    </div>
+</body>
+</html>
