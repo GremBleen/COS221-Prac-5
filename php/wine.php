85638989d436a62a23fa44ad81465713883bf60f
Morgan90-B <u18103007@tuks.co.za>
2023-06-01 00:35:56 +0200

    added wine page styling

---
+++ php/wine.php
@@ -0,0 +1,103 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Wine</title>
+</head>
+<body>
+    <?php
+    require_once("navbar.php");
+    ?>
+    <div class="container-a">
+        <div class="controls">
+            <div>
+                <input type="search" name="search" id="search" placeholder="Search for">
+            </div>
+            <div id="filter">
+                <p>Filter by</p>
+            </div>
+        </div>
+        <div class="wine-cards">
+            <div class="wine">
+                <div class="wine-photo">
+                    <img src="../img/pexels-kenneth-2912108.jpg" alt="wine photo" height="340" width="200">
+                </div>
+                <div class="specification">
+                    <h3 class="wine-name">Wine Name</h3>
+                    <h4 class="producers">Produced by winery</h4>
+                    <h4 class="region">Region</h4>
+                    <ul>
+                        <li>attribute 1</li>
+                        <li>attribute 2</li>
+                    </ul>
+                    <h4 class="rating">Rating</h4>
+                </div>
+            </div>
+            <div class="wine">
+                <div class="wine-photo">
+                    <img src="../img/pexels-kenneth-2912108.jpg" alt="wine photo" height="340" width="200">
+                </div>
+                <div class="specification">
+                    <h3 class="wine-name">Wine Name</h3>
+                    <h4 class="producers">Produced by winery</h4>
+                    <h4 class="region">Region</h4>
+                    <ul>
+                        <li>attribute 1</li>
+                        <li>attribute 2</li>
+                    </ul>
+                    <h4 class="rating">Rating</h4>
+                </div>
+            </div>
+            <div class="wine">
+                <div class="wine-photo">
+                    <img src="../img/pexels-kenneth-2912108.jpg" alt="wine photo" height="340" width="200">
+                </div>
+                <div class="specification">
+                    <h3 class="wine-name">Wine Name</h3>
+                    <h4 class="producers">Produced by winery</h4>
+                    <h4 class="region">Region</h4>
+                    <ul>
+                        <li>attribute 1</li>
+                        <li>attribute 2</li>
+                    </ul>
+                    <h4 class="rating">Rating</h4>
+                </div>
+            </div>
+            <div class="wine">
+                <div class="wine-photo">
+                    <img src="../img/pexels-kenneth-2912108.jpg" alt="wine photo" height="340" width="200">
+                </div>
+                <div class="specification">
+                    <h3 class="wine-name">Wine Name</h3>
+                    <h4 class="producers">Produced by winery</h4>
+                    <h4 class="region">Region</h4>
+                    <ul>
+                        <li>attribute 1</li>
+                        <li>attribute 2</li>
+                    </ul>
+                    <h4 class="rating">Rating</h4>
+                </div>
+            </div>
+            <div class="wine">
+                <div class="wine-photo">
+                    <img src="../img/pexels-kenneth-2912108.jpg" alt="wine photo" height="340" width="200">
+                </div>
+                <div class="specification">
+                    <h3 class="wine-name">Wine Name</h3>
+                    <h4 class="producers">Produced by winery</h4>
+                    <h4 class="region">Region</h4>
+                    <ul>
+                        <li>attribute 1</li>
+                        <li>attribute 2</li>
+                    </ul>
+                    <h4 class="rating">Rating</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
